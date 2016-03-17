ccb5f2a05b1d4eb2b0ca759f61fb5bfc65c0a0d4
esraa saad <esraa.saad18193@gmail.com>
2016-03-17 12:48:14 +0200

    HOME

---
+++ front-page.php
@@ -0,0 +1,178 @@
+<?php get_header(); ?>
+<div class="container">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="<?php bloginfo('template_url');?>/images/index_slide01.jpg" alt="WE ARE FASHION REVOLUTION!!">
+    </div>
+
+    <div class="item">
+      <img src="<?php bloginfo('template_url');?>/images/index_slide02.jpg" alt="FASHION WEEK!">
+    </div>
+
+    <div class="item">
+      <img src="<?php bloginfo('template_url');?>/images/index_slide03.jpg" alt="Get free shipping on a total order value of 600 EGP on all fashion items">
+    </div>
+
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div><hr>
+				
+		<div class="jumbotron">
+           <div class=" col-lg-4 img"> 
+
+		     <img src="<?php bloginfo('template_url'); ?>/images/women.jpg" alt="women">
+		   </div>
+       <!-- // -->
+    <?php
+    $args = array(
+      'post_type' => 'products',
+      'orderby'   =>'id',
+      'order'   =>'DESC',
+      'posts_per_page'=>4,
+      'tax_query' =>
+          array(
+          'taxonomy' => 'woman_clothes',
+          ),
+          'relation' => 'AND',
+          array(
+          'taxonomy' => 'woman_accessories',
+          'field'    => 'slug',
+          ),
+          'relation' => 'AND',
+          array(
+          'taxonomy' => 'woman_accessories',
+          'field'    => 'slug',
+          ),
+     );
+          $query = new WP_Query( $args );
+           
+           if($query->have_posts()):
+            while ($query->have_posts()):
+              $query->the_post();
+            ?>
+              <div class="col-md-4">
+                <div class="thumbnail">
+                <a href="<?php the_permalink()?>">  <?php  the_post_thumbnail('thumbnail'); ?></a>
+                  <div class="caption">
+                    <h3><?php the_title() ?></h3>
+                     <p><a href="#" class="btn btn-success btn-block" role="button">Buy Now</a> </p>
+
+                  </div>
+                </div>
+              </div>
+    <?php
+  endwhile;
+ endif;
+
+?>    
+		</div><br><hr><br>
+    	
+    
+    
+
+    <div class="jumbotron ">
+     <div class=" col-lg-4 img"> 
+    <img src="<?php bloginfo('template_url'); ?>/images/men.jpg">
+    </div>
+     <?php
+
+          $args = array(
+            'post_type' => 'products',
+            
+            'order'   =>'DESC',
+            'posts_per_page'=>4,
+            'tax_query' =>
+                array(
+                'taxonomy' => 'man_clothes',
+                ),
+                'relation' => 'AND',
+                array(
+                'taxonomy' => 'man_shoes',
+                // 'field'    => 'slug',
+                ),
+                'orderby'   =>'id',
+           );
+          $query = new WP_Query( $args );
+           
+           if($query->have_posts()):
+            while ($query->have_posts()):
+              $query->the_post();
+            ?>
+              <div class="col-md-4">
+                <div class="thumbnail">
+                <a href="<?php the_permalink()?>">  <?php  the_post_thumbnail('thumbnail'); ?></a>
+                  <div class="caption">
+                    <h3><?php the_title() ?></h3>
+                    <p><a href="#" class="btn btn-success btn-block" role="button">Buy Now</a> </p>
+
+                  </div>
+                </div><br>
+              </div>
+    <?php
+  endwhile;
+ endif;
+
+?> 
+   
+    </div>      </div><br><hr><br>
+
+    <!-- // -->
+    <div class="jumbotron ">
+           <div class=" col-lg-4 img"> 
+
+      <img src="<?php bloginfo('template_url'); ?>/images/kids.jpg">
+    </div>
+      <?php
+    $args = array(
+      'post_type' => 'products',
+      'orderby'   =>'id',
+      'order'   =>'DESC',
+      'posts_per_page'=>4,
+      'tax_query' =>
+          array(
+          'taxonomy' => 'kides_clothes',
+          ),
+     );
+        $query = new WP_Query( $args );
+         
+         if($query->have_posts()):
+          while ($query->have_posts()):
+            $query->the_post();
+          ?>
+             <div class="col-md-4">
+              <div class="thumbnail">
+              <a href="<?php the_permalink()?>">  <?php  the_post_thumbnail('thumbnail'); ?></a>
+                <div class="caption">
+                  <h3><?php the_title() ?></h3>
+                    <p><a href="#" class="btn btn-success btn-block" role="button">Buy Now</a> </p>
+                </div>
+              </div>
+            </div>
+    <?php
+  endwhile;
+ endif;
+?> 
+  </div>    </div><br><hr><br>
+
+ <!-- end container -->
+<?php get_footer(); ?>
