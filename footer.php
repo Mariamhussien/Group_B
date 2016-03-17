10126bb6b2879a7e03232495a1936dabc728aa6e
esraa saad <esraa.saad18193@gmail.com>
2016-03-17 12:46:06 +0200

    THE FOOTER

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<footer>
+    <div class=" col-lg-12 footer">
+        <p> Copyright 2016</p>
+    </div>
+</footer>
+<script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+
+		</div>
+	</body>
+</html>
